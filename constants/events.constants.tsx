e1246f6d79ae3fd135fa9e26f0a62420a3a42f54
Eimi <lemito66@gmail.com>
2025-12-25 20:22:07 -0500

    Update event image from chimborazo-ploma.jpg to sultana.jpg

--- constants/events.constants.tsx
+++ constants/events.constants.tsx
@@ -12,7 +12,7 @@ export const EVENTS: Event[] = [
     description: [
       `Un evento urbano lleno de energía y propósito, con distancias de 5 km y 10 km, que se realizará en Riobamba el sábado 17 de enero de 2026 a las 20h00. Una carrera nocturna que une a la comunidad en torno a la solidaridad, el movimiento y el orgullo de nuestra tierra. Un recorrido donde cada kilómetro se convierte en un acto de apoyo, esperanza y transformación social.`,
     ],
-    image: "/chimborazo-ploma.jpg",
+    image: "/sultana.jpg",
     inscriptionLink: "https://forms.gle/iaQSyQfdXALnW1Fr8",
   },
   {
